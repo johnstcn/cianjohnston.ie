fddf54988cd8f166dee4892df55849a35b4c65e8
Cian Johnston <public@cianjohnston.ie>
2018-05-06 17:14:51 +0100

    fixes

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-set -euo pipefail
 
 echo "Cleaning previously generated files"
 rm -rf ./public
@@ -8,4 +7,4 @@ echo "Generating static files"
 hugo
 
 echo "Running rsync"
-rync -avz public/ cianjohnston.ie:/home/user-data/www/default
+rsync -avz public/ cianjohnston.ie:/home/user-data/www/default
